380af4bfd9b0e0d978117f24e31c69732ddb0e3f
Nathan-ma <than.matos@gmail.com>
2022-08-10 12:36:31 -0300

    Create test_script.sh

---
+++ test_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function check_env_setup_update() {
+  local path="${BASH_SOURCE:-$0}"
+	local remote_md5sum=$(curl -sL https://raw.githubusercontent.com/Nathan-ma/check_update_script/master/test_script.sh | md5sum | cut -d ' ' -f 1)
+	local local_md5sum=$(md5sum $path | cut -d ' ' -f 1)
+
+  # return the comparison's result
+  [ $remote_md5sum == $local_md5sum ]
+  return
+}
+
+check_env_setup_update
+update_result=$?
+if ! (exit $update_result); then
+  echo "Content is different, this should be an update"
+fi
