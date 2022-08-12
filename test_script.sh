506fb963a90166ee1f3fc128099839e20a9137a3
Nathan-ma <than.matos@gmail.com>
2022-08-12 16:43:46 -0300

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/usr/bin/env bash
 
 # Check to make sure script is being sourced otherwise exit
 SOURCED=0
@@ -22,22 +22,26 @@ else
 fi
 
 
-function check_env_setup_update() {
-  local path="${BASH_SOURCE:-$0}"
-	local remote_md5sum=$(curl -sL https://raw.githubusercontent.com/Nathan-ma/check_update_script/main/test_script.sh | md5sum | cut -d ' ' -f 1)
-	local local_md5sum=$(md5sum $path | cut -d ' ' -f 1)
+SCRIPT_PATH="${BASH_SOURCE[0]:-${(%):-%x}}"
 
+check_env_setup_update() {
+
+	local remote_md5sum=$(curl -sL https://raw.githubusercontent.com/Nathan-ma/check_update_script/main/test_script.sh | md5sum | cut -d ' ' -f 1)
+	local local_md5sum=$(md5sum "$1" | cut -d ' ' -f 1)
+  echo "$local_md5sum"
+  echo "$remote_md5sum"
   # return the comparison's result
-  [ $remote_md5sum == $local_md5sum ]
+  [[ "$remote_md5sum" == "$local_md5sum" ]]
   return
 }
 
-check_env_setup_update
+check_env_setup_update $SCRIPT_PATH
 update_result=$?
 if ! (exit $update_result); then
-  [ "$SOURCED" -eq 1 ] && echo "Sourced" || echo "Not Sourced"
   echo "Content is different, this should be an update"
-  wget "https://raw.githubusercontent.com/Nathan-ma/check_update_script/main/test_script.sh" -O "test_script.sh"
-
+  wget "https://raw.githubusercontent.com/Nathan-ma/check_update_script/main/test_script.sh" -O "test_script.tmp"
+  source test_script.tmp
+  mv test_script.tmp "$PWD/$SCRIPT_PATH"
 fi
 
+[ "$SOURCED" -eq 1 ] && echo "Sourced" || echo "Not Sourced"
